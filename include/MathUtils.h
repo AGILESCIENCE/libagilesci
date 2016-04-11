bcb9079351338941189c0780a8746a46776cc2b9
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-04-11 16:32:54 +0200

    Revert "Cache the last SphDstDeg computation."
    This reverts commit c69713e6b04b32d58612f045a67d978b79cf21bb.
    
    Conflicts:
    	include/MathUtils.h

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -32,13 +32,6 @@ inline double Sinaa(double angle)
 
 inline double SphDistDeg(double long1, double lat1, double long2, double lat2)
 {
-    // cache the last computation
-    static double prev_long1=0.0, prev_long2=0.0, prev_lat1=0.0, prev_lat2=0.0;
-    static double prev_dist = 0.0;
-    if(prev_long1 == long1 && prev_long2 == long2 &&
-       prev_lat1 == lat1 && prev_lat2 == lat2)
-        return prev_dist;
-
     double l1 = long1*DEG2RAD;
     double l2 = long2*DEG2RAD;
     double b1 = lat1*DEG2RAD;
@@ -46,16 +39,7 @@ inline double SphDistDeg(double long1, double lat1, double long2, double lat2)
     double val = sin(b1)*sin(b2) + cos(b1)*cos(b2) *cos(l1-l2);
     if(val > 1.0) return 0.0;
     double dist = acos(val);
-    dist*=RAD2DEG;
-
-    // update the cache
-    prev_dist=dist;
-    prev_long1=long1;
-    prev_long2=long2;
-    prev_lat1=lat1;
-    prev_lat2=lat2;
-
-    return dist;
+    return dist*RAD2DEG;
 }
 
 void Euler(double ai, double bi, double * ao, double * bo, int select);
