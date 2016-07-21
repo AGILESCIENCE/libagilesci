4171d4e46ee2731cab19a0bfb56eb6df569d160f
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-07-21 11:54:06 +0200

    Fix SphDistDeg border case.

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -38,6 +38,7 @@ inline double SphDistDeg(double long1, double lat1, double long2, double lat2)
     double b2 = lat2*DEG2RAD;
     double val = sin(b1)*sin(b2) + cos(b1)*cos(b2) *cos(l1-l2);
     if(val > 1.0) return 0.0;
+    if(val < -1.0) return 0.0;
     double dist = acos(val);
     return dist*RAD2DEG;
 }
