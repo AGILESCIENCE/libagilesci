013fe2347bbcb8b69afa81eea94ba981f3358e42
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-02-26 16:08:03 +0100

    useedp second bug correction

--- src/CalibUtils.cpp
+++ src/CalibUtils.cpp
@@ -315,7 +315,7 @@ for (int thetaind = 0; thetaind < numtheta; thetaind++) {
 			if (m_hasEdp) {
 				/// Calcolo della dispersione energetica totale per ogni canale di energia
 				for (int eobs = iMin;  eobs <= iMax; eobs++) {
-					edpArr[etrue] += m_edp.Val(m_energy[etrue], m_energy[eobs], m_theta[thetaind], m_phi[phiind]);
+					edpArr[etrue] += m_edp.Val(m_energy[eobs], m_energy[etrue], m_theta[thetaind], m_phi[phiind]);
 					cout << "EDP: " << etrue << " " << m_energy[etrue] << " " << eobs << " " << m_energy[eobs] << " " << thetaind << " " << m_theta[thetaind] << " " << phiind << " " << m_phi[phiind] << " " << edpArr[etrue] << endl;
 				}
 				cout << "FINAL EDP etrue: " << m_energy[etrue] << " " << edpArr[etrue] << endl;
