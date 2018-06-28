e4b3874b0a124ac6a7169326c874ce5f79a1ea18
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-06-28 16:49:36 +0200

    par limits out

--- src/RoiMulti5.cpp
+++ src/RoiMulti5.cpp
@@ -3639,7 +3639,7 @@ for (int i=0; i<m_srcCount; ++i) {
 		continue;
 	string srcoutname(string(fileName) + "_" + m_sources[i].GetLabel() + ".source");
 	ofstream srcout(srcoutname.c_str());
-	srcout << "! Label Fix index ULConfidenceLevel SrcLocConfLevel start_l start_b start_flux [ lmin,  lmax ] [ bmin, bmax ] typefun par2 par3 galmode2 galmode2fit isomode2 isomode2fit edpcor fluxcor integratortype expratioEval expratio_minthr expratio_maxthr expratio_size " << endl;
+	srcout << "! Label Fix index ULConfidenceLevel SrcLocConfLevel start_l start_b start_flux [ lmin,  lmax ] [ bmin, bmax ] typefun par2 par3 galmode2 galmode2fit isomode2 isomode2fit edpcor fluxcor integratortype expratioEval expratio_minthr expratio_maxthr expratio_size [ index_min , index_max ] [ par2_min , par2_max ] [ par3_min , par3_max ]" << endl;
 	srcout << "! sqrt(TS)" << endl ;
 	srcout << "! L_peak B_peak Dist_from_start_position" << endl;
 	const Ellipse& ellipse = m_sources[i].GetEllipse();
@@ -3695,7 +3695,13 @@ for (int i=0; i<m_srcCount; ++i) {
 	srcout << " " << minThreshold;
 	srcout << " " << maxThreshold;
 	srcout << " " << squareSize;
-
+	srcout << " [ " << m_inSrcDataArr[i].index_low_limit;
+	srcout << " , " << m_inSrcDataArr[i].index_upp_limit;
+	srcout << " ] [ " << m_inSrcDataArr[i].par2_low_limit;
+	srcout << " , " << m_inSrcDataArr[i].par2_upp_limit;
+	srcout << " ] [ " << m_inSrcDataArr[i].par3_low_limit;
+	srcout << " , " << m_inSrcDataArr[i].par3_upp_limit;
+	srcout << " ] ";
 	srcout	<< endl;
 	/// zzz m_sources[i].PrintAbphi();
 	double dist = SphDistDeg(m_sources[i].GetSrcL(), m_sources[i].GetSrcB(), m_inSrcDataArr[i].srcL, m_inSrcDataArr[i].srcB);
